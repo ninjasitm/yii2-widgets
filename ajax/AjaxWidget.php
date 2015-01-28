c0f2ee17221112ad196d4af1ebdf3b4fb00956be
nitm <developers@ninjasitm.com>
2015-01-28 15:29:55 -0500

    Moved a few widgets from lab1

---
+++ ajax/AjaxWidget.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace nitm\widgets\ajax;
+
+use yii\helpers\Html;
+
+/**
+ * TicketMetadata widget renders info about a ticket only after the page loads
+ */
+class AjaxWidget extends \yii\base\Widget
+{	
+	/**
+	 * The url where the information should be taken from
+	 */
+	public $url;
+	
+	 /**
+	  * The text shown when loading
+	  */
+	public $placeholder = "Loading...";
+	
+	/**
+	 * The parameters to send with the url
+	 */
+	public $queryParams;
+		
+	public $options = [
+		'class' => 'text-left',
+		'role' => 'AjaxWidget'
+	];
+	/**
+	 * When should the request be pulled?
+	 */
+	public $on;
+	
+	private $_events = [
+		'documentLoad' => 'On Document Load',
+		'inViewport' => 'The element is in the vieport',
+	];
+	
+	public function init()
+	{
+		$this->options['id'] = "ajaxWidget".uniqid();
+		AjaxWidgetAsset::register($this->getView());
+	}
+	
+	public function run()
+	{
+		$script = '';
+		$this->options['data-type'] = $this->on;
+		$this->options['data-url'] = $this->url;
+		$this->options['data-query-params'] = json_encode($this->queryParams);
+		$this->options['role'] = isset($this->options['role']) ? $this->options['role'].' AjaxWidget' : 'AjaxWidget';
+		switch($this->on)
+		{	
+			case 'inViewport':
+			$this->options['data-on-scrolled-in'] = $this->options['id'];
+			$this->options['role'] .= ' onScrolledIntoView';
+			break;
+		}
+		return Html::tag('div', Html::tag('span', $this->placeholder), $this->options);
+	}
+}
---
+++ ajax/assets/AjaxWidgetAsset.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace nitm\widgets\ajax\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AjaxWidgetAsset extends AssetBundle
+{
+	public $sourcePath = '@nitm/widgets/ajax/assets';
+	public $css = [
+	];
+	public $js = [
+		'js/ajax-widget.js'
+	];
+	public $jsOptions = ['position' => \yii\web\View::POS_END];
+	public $depends = [
+		'nitm\assets\AppAsset',
+	];
+	
+	public function initView($view)
+	{
+		$files = [
+			pathinfo('@nitm/assets/js/jquery-plugins/jquery.ui.widget.js'),
+			pathinfo('@nitm/assets/js/jquery-plugins/jquery-ui-scrollable/jquery-ui-scrollable.js')
+		];
+		foreach($files as $f)
+		{
+			$asset = new \yii\web\AssetBundle([
+				'sourcePath' => $f['dirname'],
+				'js' => [$f['basename']],
+			]);
+			$asset->publish($view->getAssetManager());
+			$view->assetBundles[static::className().'\\'.$f['basename']] = $asset;
+		}
+		return $this;
+	}
+}
